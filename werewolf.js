17aa41d53600723fde00946fe447cc3bd4f49b8c
Robert Shenton <theshenton@gmail.com>
2020-07-24 14:50:24 +0200

    Add in game loop

--- werewolf.js
+++ werewolf.js
@@ -8,14 +8,10 @@ function generate() {
     let namesInput = document.getElementById("player-names").value;
     names = namesInput.split("\n");
     villagers = [...names];
-
-    werewolfs = [];
-    seers = [];
-    doctors = [];
     
-    const numWerewolfs = 1;
-    const numDoctors = 1;
-    const numSeers = 1;
+    const numWerewolfs = document.getElementById("werewolfCount").value;
+    const numDoctors = document.getElementById("doctorCount").value;
+    const numSeers = document.getElementById("seerCount").value;
 
     for (var i = 0; i < numWerewolfs; i++)
     {
@@ -48,11 +44,14 @@ function generate() {
             type = 'doctor';
         
         addPlayer(name, type);
-    })
+    });
+
+    document.getElementById("setup").style.display = "none";
+    document.getElementById("game").style.display = "block";
 }
 
 function addPlayer(name, type) {
-    let gameContainer = document.getElementById("game");
+    let gameContainer = document.getElementById("players");
     let playerContainer = document.createElement("div");
     playerContainer.classList.add("player");
     playerContainer.innerHTML = '<img src="images/' + type + '.svg" /><h5>' + name + '</h5>';
@@ -66,7 +65,20 @@ function addPlayer(name, type) {
     });
 }
 
+function endGame() {
+    werewolfs = [];
+    seers = [];
+    doctors = [];
+    villagers = [];
+
+    document.getElementById("players").innerHTML = "";
+    document.getElementById("setup").style.display = "grid";
+    document.getElementById("game").style.display = "none";
+}
+
 window.onload = function() {
     var startButton = document.getElementById("start");
     startButton.addEventListener("click", generate);
+    var endButton = document.getElementById("end");
+    endButton.addEventListener("click", endGame);
 }
